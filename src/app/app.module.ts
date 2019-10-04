e5bb54f9ccf3c4b0b9908471ba11d92da7b8dffb
Cristiano Tofani <cri.tofani@gmail.com>
2019-10-04 17:22:37 +0200

    Setup bootstrap italia css and js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestDirectiveDirective } from './shared/directives/test-directive.directive';
-import { TestPipe } from './shared/pipes/test.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestDirectiveDirective,
-    TestPipe
   ],
   imports: [
     BrowserModule,
